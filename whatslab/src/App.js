03e01c6abc2b21abb72052f1d3bad5c6eed9afcb
nathaliacardoso <nathalia.mscardoso@gmail.com>
2021-03-12 17:13:47 -0300

    finalizar inputs

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const arrayMensagem =[];
 
-export default App;
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 180px;
+  width: 240px;
+  border: 1px solid gray;
+  margin: 20px;
+  padding: 30px;
+`;
+class App extends React.Component {
+    state = {
+      mensagens: arrayMensagem,
+      usuarioNome: '',
+      usuarioMensagem: ''
+    };
+
+  changeUsuarioNome = (event) => {
+    this.setState({ usuarioNome: event.target.value });
+  };
+
+  changeUsuarioMensagem = (event) => {
+    this.setState({ usuarioMensagem: event.target.value });
+  };
+
+  addNovaMensagem = () => {
+    const novaMensagem = {
+      nomeUsuario: this.state.usuarioNome,
+      mensagemUsuario: this.state.usuarioMensagem
+    };
+    this.setState({
+      mensagens: [...this.state.mensagens, novaMensagem],
+      usuarioNome: "",
+      usuarioMensagem:""
+    });
+  };
+
+  render() {
+    const componentesMensagem = this.state.mensagens.map((p) => {
+      return (
+        <div
+          nomeUsuario={p.nomeUsuario}
+          mensagemUsuario={p.mensagemUsuario}
+        />  
+      );
+    });
+      return (
+        <AppContainer>
+          <FormContainer>
+            <input
+              placeholder={"Usuário"}
+              value={this.state.usuarioNome}
+              onChange={this.nomeUsuario}
+            />
+            <input
+              placeholder={"Mensagem"}
+              value={this.state.usuarioMensagem}
+              onChange={this.mensagemUsuario}
+            />
+            <button onClick={this.addNovaMensagem}>Enviar</button>
+          </FormContainer>
+          {componentesMensagem}
+      </AppContainer>
+        );
+    }   
+  } 
+  
+    export default App;
